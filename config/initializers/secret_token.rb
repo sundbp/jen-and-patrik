3cd1da5e97c02f8c96c87ff08bebced38480e533
Patrik Sundberg <patrik.sundberg@gmail.com>
2012-01-02 18:04:10 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JenAndPatrik::Application.config.secret_token = '61dd7c0ef247515515d99e6789ca5dff39d6cc93d27e4dce72faedb0309a9c373e37d0db4765cb3f5db60253d8ccc80b97a1e6a798c90f17a6c8af00682ceee1'
